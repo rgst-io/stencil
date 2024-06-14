611f320e57a9f916b2a3388481cfbacc42c19d47
Jared Allard <jaredallard@users.noreply.github.com>
2024-06-14 13:06:20 -0700

    ci: allow setting next version

--- .github/scripts/get-next-version.sh
+++ .github/scripts/get-next-version.sh
@@ -2,10 +2,21 @@
 # Determines the next version.
 set -euo pipefail
 
+# VERSION_OVERRIDE is an environment variable that allows the next
+# version to be overridden. If VERSION_OVERRIDE is set, the next version
+# will be the value of VERSION_OVERRIDE.
+VERSION_OVERRIDE=${VERSION_OVERRIDE:=}
+
 # BUILD_RC is an environment variable that determines whether the build
 # is a release candidate. If BUILD_RC is set to true, the next version
 # will be a release candidate version (-rc.X).
 BUILD_RC=${BUILD_RC:=false}
+
+if [[ -n "$VERSION_OVERRIDE" ]]; then
+  echo "Overriding next version with: $VERSION_OVERRIDE" >&2
+  echo "$VERSION_OVERRIDE"
+  exit 0
+fi
 
 # Determine the next version as reported by the next-version command.
 next_version=$(mise run next-version 2>/dev/null | sed 's/-rc.*//' | tr -d '\n')
