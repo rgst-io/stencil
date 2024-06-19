eae8eacd7bc57ade4ef0e7f8d854024f0781efa8
Jared Allard <jaredallard@users.noreply.github.com>
2024-06-18 19:17:47 -0700

    build: upload apt/rpm/apk packages to fury.io for publishing (#107)
    We're going to use `fury.io` to host our packages so users don't have to
    manually download a `deb`, `rpm`, `apk` file everytime.

---
+++ .github/scripts/fury-upload.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Originally adapated from:
+# https://github.com/goreleaser/goreleaser/blob/main/scripts/fury-upload.sh
+set -euo pipefail
+
+# FURY_PUSH_TOKEN is a secret token that is used to push packages to
+# fury (https://fury.io). It is a required environment variable.
+FURY_PUSH_TOKEN="${FURY_PUSH_TOKEN}"
+
+# allowed_exts is an array of allowed file extensions that can be
+# uploaded to fury.
+allowed_exts=("deb" "rpm" "apk")
+
+# Get the file extension
+file_ext="${1##*.}"
+
+is_allowed=false
+for ext in "${allowed_exts[@]}"; do
+  if [[ "$ext" == "$file_ext" ]]; then
+    is_allowed=true
+    break
+  fi
+done
+
+if [[ "$is_allowed" == "false" ]]; then
+  exit 0
+fi
+
+cd dist
+echo "uploading $1"
+status="$(curl -s -q -o /dev/null -w "%{http_code}" -F package="@$1" "https://${FURY_PUSH_TOKEN}@push.fury.io/rgst-io/")"
+echo "got: $status"
+if [[ "$status" == "200" ]] || [[ "$status" == "409" ]]; then
+  exit 0
+fi
+
+# Otherwise, exit with an error
+exit 1
