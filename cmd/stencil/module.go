e4a0e8b4e4ddadfb008266c328ee79aaa5f61e12
Jared Allard <jaredallard@users.noreply.github.com>
2025-07-07 06:42:50 -0700

    feat: add module gen-args-docs command for module doc gen (#308)

--- cmd/stencil/module.go
+++ cmd/stencil/module.go
@@ -1,16 +1,20 @@
-// Copyright (C) 2024 stencil contributors
+// Copyright (C) 2025 stencil contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//        http://www.apache.org/licenses/LICENSE-2.0
+//         http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Description: This file contains code for the module command
 
 package main
 
@@ -19,12 +23,15 @@ import (
 	"go.rgst.io/stencil/v2/pkg/slogext"
 )
 
-// NewCreateModuleCommand returns a new urfave/cli.Command for the
-// create module command.
-func NewCreateModuleCommand(log slogext.Logger) *cli.Command {
-	c := NewModuleCreateCommand(log)
-	c.Name = "module"
-	c.Hidden = true
-	c.Description = "DEPRECATED: Use 'module create' instead. " + c.Description
-	return c
+// NewModuleCommand returns a new [cli.Command] for the
+// module command
+func NewModuleCommand(log slogext.Logger) *cli.Command {
+	return &cli.Command{
+		Name:        "module",
+		Description: "Commands related to interacting with modules",
+		Commands: []*cli.Command{
+			NewModuleCreateCommand(log),
+			NewModuleGenArgsDocsCommand(log),
+		},
+	}
 }
